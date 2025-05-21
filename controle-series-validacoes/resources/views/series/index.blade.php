e5cde0c2190aeba9b68ff0d9fd1e18a30a0c38c5
rafaelpllopes <rafael.pllopes@gmail.com>
2025-05-21 16:04:39 -0300

    Implementa do o deletar (destroy).

--- controle-series-validacoes/resources/views/series/index.blade.php
+++ controle-series-validacoes/resources/views/series/index.blade.php
@@ -2,7 +2,13 @@
     <a class="btn btn-dark mb-2" href="{{ route('series.create') }}">Adicionar</a>
     <ul class="list-group" >
         @foreach ($series as $serie)
-        <li class="list-group-item" >{{ $serie->nome }}</li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">{{ $serie->nome }}
+            <form action={{ route('series.destroy', $serie->id) }} method="post">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-sm">X</button>
+            </form>
+        </li>
         @endforeach
     </ul>
 </x-layout>
--- controle-series-validacoes/routes/web.php
+++ controle-series-validacoes/routes/web.php
@@ -7,9 +7,12 @@ Route::get('/', function () {
     return redirect('series');
 });
 
-Route::resource('/series', SeriesController::class);
+Route::resource('/series', SeriesController::class)
+    ->only(['index', 'create', 'store', 'destroy']);
 // Route::controller(SeriesController::class)->group(function () {
 //     Route::get('/series', 'index')->name('series.index');
 //     Route::get('/series/criar')->name('series.create');
 //     Route::post('/series/store')->name('series.store');
 // });
+
+// Route::delete('/series/destroy/{serie}', [SeriesController::class, 'destroy'])->name('series.destroy');
