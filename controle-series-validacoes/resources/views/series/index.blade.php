cee4b1c43ce904388fb9c9b490c67e959dea8107
rafaelpllopes <rafael.pllopes@gmail.com>
2025-05-22 09:54:13 -0300

    Atualizado a View series.index

--- controle-series-validacoes/resources/views/series/index.blade.php
+++ controle-series-validacoes/resources/views/series/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Séries">
-    <a class="btn btn-dark mb-2" href="{{ route('series.create') }}">Adicionar</a>
+    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
 
     @isset($mensagemSucesso)
     <div class="alert alert-success">
@@ -7,15 +7,24 @@
     </div>
     @endisset
 
-    <ul class="list-group" >
+    <ul class="list-group">
         @foreach ($series as $serie)
-        <li class="list-group-item d-flex justify-content-between align-items-center">{{ $serie->nome }}
-            <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">E</a>
-            <form action={{ route('series.destroy', $serie->id) }} method="post" class="ms-2">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-danger btn-sm">X</button>
-            </form>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            {{ $serie->nome }}
+
+            <span class="d-flex">
+                <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                    E
+                </a>
+
+                <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
+                </form>
+            </span>
         </li>
         @endforeach
     </ul>
